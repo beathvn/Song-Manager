c9e4f6181761432f4a644a33f6f7f38816ab6fed
Lukas Ellemunt <lukas.ellemunt@icloud.com>
2023-06-27 19:18:09 +0200

    added functionality to utils.py * query_yes_no * open_file

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,59 @@
 # This file contains some utility functions that are used in multiple places in the project.
 
+
+# system imports
+import os
+import platform
+import subprocess
+import sys
+
+
 def print_divider():
     print('--------------------------')
+
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write(
+                "Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+            
+
+def open_file(filepath: str)-> None:
+    """Opens the file specified in filepath. Handles the different platforms (windows, mac...)
+
+    Args:
+        filepath (str): path you want to open
+    """
+    if platform.system() == 'Darwin':
+        subprocess.call(('open', filepath))
+    elif platform.system() == 'Windows':
+        os.startfile(filepath)
+    else:  # linux variants
+        subprocess.call(('xdg-open', filepath))
