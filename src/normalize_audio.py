74e6c26c0617798fd5b8ca4ebe93e9480594e03c
Lukas Ellemunt <lukas.ellemunt@icloud.com>
2023-07-04 07:30:02 +0200

    added feature to normalize_audio.py now the directories get created automatically the songs which correspond to a given keyword are copied in auto

--- src/normalize_audio.py
+++ src/normalize_audio.py
@@ -7,6 +7,7 @@
 # system imports
 from argparse import ArgumentParser
 import os
+import shutil
 import sys
 sys.path.append('../src/')
 
@@ -15,16 +16,32 @@ from pydub import AudioSegment
 from pydub.effects import normalize
 from pydub.utils import mediainfo
 from tqdm import tqdm
+import pandas as pd
 
 # user imports
 from helpers.logger import logger
+import helpers.dataloading as dataloading
 
 
-def main(args):
-    source_folder = args.source
-    destination_folder = args.destination
+def copy_leise_songs_to_directory(xml_file: str, destination_folder: str, keyword: str):
+    logger.info(f'Searcihng the {xml_file} for songs with the keyword {keyword}.')
+    data_dict = dataloading.get_dict_from_xml(xml_file)
+    data = pd.DataFrame(data_dict['DJ_PLAYLISTS']['COLLECTION']['TRACK'])
 
-    files = [f for f in os.listdir(source_folder) if os.path.isfile(os.path.join(source_folder, f)) and not f.startswith('.')]
+    location_series = data[data['@Comments'].str.contains(keyword, case=False)]['@Location']
+    location_list = location_series.apply(lambda x: x[16:].replace('%20', ' ').replace(
+        '%26', '&').replace('%27', '\'').replace('%c3%ab', 'ë').replace('%5b', '[').replace('%5d', ']').replace('%c3%b6', 'ö').replace('%c3%bc', 'ü')).to_list()
+    
+    logger.info(f'Found {len(location_list)} songs that have the keyword leise in it.')
+    logger.info(f'Copy those songs to {destination_folder}.')
+
+    for file in tqdm(location_list):
+        shutil.copy(file, destination_folder)
+
+    return
+    
+
+def normalize_files(files: list, source_folder, destination_folder):
 
     for file in tqdm(files, desc='Normalizing Audio', unit='song'):
         # Load the audio file
@@ -47,21 +64,41 @@ def main(args):
                                 format=curr_format, bitrate=current_bitrate)
 
 
+def main(args):
+    logger.info('Start of program: Normalize Audio')
+    source_folder = args.source
+    destination_folder = args.destination
+    xml_file = args.xmlfile
+
+    if not os.path.exists(args.source):
+        os.mkdir(args.source)
+    else:
+        logger.warning(f'Folder {source_folder} already exists. Aborting...')
+        raise FileExistsError(f'Folder {source_folder} already exists. Aborting...')
+    if not os.path.exists(args.destination):
+        os.mkdir(args.destination)
+    else:
+        logger.warning(f'Folder {destination_folder} already exists. Aborting...')
+        raise FileExistsError(f'Folder {destination_folder} already exists. Aborting...')
+    
+    # copy files into a new folder
+    copy_leise_songs_to_directory(xml_file, source_folder, 'leise')
+
+    files = [f for f in os.listdir(source_folder) if os.path.isfile(os.path.join(source_folder, f)) and not f.startswith('.')]
+    # normalize_files(files, source_folder, destination_folder)
+
+    logger.info('End of program: Normalize Audio\n')
+
 
 if __name__ == "__main__":
-    logger.info('Start of program: Normalize Audio')
 
     parser = ArgumentParser()
     parser.add_argument('-s', '--source')
     parser.add_argument('-d', '--destination')
+    parser.add_argument('-x', '--xmlfile')
 
     args = parser.parse_args()
-    if os.path.exists(args.source) and os.path.exists(args.destination):
-        main(args)
-    else:
-        raise FileNotFoundError('One of the provided directories does not exist. ' +
-                                f'Please create them. You passed: {args.source} and {args.destination}')
-    
-    logger.info('End of program: Normalize Audio\n')
+
+    main(args)
 
     #TODO: I want to open the logfile, just if there were warnings
